1bbcd5e461b39d73a76d24ecfe351bb91c971822
Vaibhav <vaibhavsonim26@gmail.com>
2022-10-29 22:42:47 +0530

    ci: Added Eslint for unused styles and added Poppins font

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react', 'react-native'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    curly: 0,
+  },
 };
