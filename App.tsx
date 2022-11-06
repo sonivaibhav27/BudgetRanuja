7b87835a9ead26b07f1489b20aa185652d20f12f
Vaibhav <vaibhavsonim26@gmail.com>
2022-11-06 21:33:54 +0530

    ci: Increase Target Sdk version and include ios admob id And target Android 12

--- App.tsx
+++ App.tsx
@@ -1,4 +1,18 @@
-import {withCodePush, withRecoil} from './src/hooks';
-import Navigation from './src/navigations';
+import 'react-native-gesture-handler';
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {RecoilRoot} from 'recoil';
+import {withCodePush} from './src/app/hof';
+import NavigationRoot from './src/app/navigation';
+import CommonStyles from './src/app/styles';
 
-export default withRecoil(withCodePush(Navigation));
+export default withCodePush(() => {
+  return (
+    <SafeAreaView style={CommonStyles.flex1}>
+      <StatusBar backgroundColor="#f1f1f1" barStyle="dark-content" />
+      <RecoilRoot>
+        <NavigationRoot />
+      </RecoilRoot>
+    </SafeAreaView>
+  );
+});
--- index.js
+++ index.js
@@ -1,44 +1,11 @@
 /**
  * @format
  */
+import './src/app/extensions/number.extension';
 import {AppRegistry} from 'react-native';
-import * as Sentry from '@sentry/react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {
-  BudgetBillModel,
-  Schema,
-  BudgetModel,
-  CategoryModel,
-} from './src/database';
-import {DatabaseConfig, Keys} from './src/config';
-import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
-import {Database} from '@nozbe/watermelondb';
-import migrations from './src/database/_init/migrations';
-import {NativeModuleFunctions, QonversionManager} from './src/utils';
+import {initializeWatermenlonDb} from './src/database';
 
-NativeModuleFunctions.initializeDebugFacebook();
-
-Sentry.init({
-  dsn: Keys.REMOTE_LOGGER_KEY,
-  enableAutoSessionTracking: true,
-  environment: __DEV__ ? 'development' : 'production',
-});
-
-QonversionManager.launchQonversionSDK();
-const adapter = new SQLiteAdapter({
-  schema: Schema,
-  jsi: false,
-  dbName: DatabaseConfig.dbName,
-  onSetUpError: error => {
-    // Database failed to load -- offer the user to reload the app or log out
-    console.log(error);
-  },
-  // migrations: [],
-});
-export const WatermenlonDB = new Database({
-  adapter,
-  modelClasses: [BudgetBillModel, BudgetModel, CategoryModel],
-});
-
+export const WatermenlonDB = initializeWatermenlonDb();
 AppRegistry.registerComponent(appName, () => App);
